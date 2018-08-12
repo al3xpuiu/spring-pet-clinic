1a1a922c83704986260769263f139602fcf8b907
Loky <puiualexandru87@yahoo.com>
2018-08-12 15:41:30 +0200

    Listed all Owners on Owners index page. closes #16

--- pet-clinic-web/src/main/java/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/springpetclinic/controllers/OwnerController.java
@@ -1,7 +1,10 @@
 package springpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springpetclinic.services.OwnerService;
 
 /**
  * Created by Loky on 08/08/2018.
@@ -10,8 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute( "ownersCollection", ownerService.findAll() );
+
         return "owners/index";
     }
 }
