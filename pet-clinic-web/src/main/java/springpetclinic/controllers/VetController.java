c8c47fc66881694873df1943bfaff03cf666adf8
Loky <puiualexandru87@yahoo.com>
2018-08-12 15:51:58 +0200

    Listed all Vets on Vets index page. closes #15

--- pet-clinic-web/src/main/java/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/springpetclinic/controllers/VetController.java
@@ -1,7 +1,10 @@
 package springpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springpetclinic.services.VetService;
 
 /**
  * Created by Loky on 08/08/2018.
@@ -10,8 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute( "vetsCollection", vetService.findAll() );
 
         return "vets/index";
     }
