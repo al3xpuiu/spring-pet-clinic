d7da7e1bf034df112e583a8f973cfbd6b1e45a05
Loky <puiualexandru87@yahoo.com>
2018-08-16 20:19:48 +0200

    Created Pet Type, Pet and Visit Entities. closes #26

--- pet-clinic-data/src/main/java/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/springpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Loky on 02/08/2018.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
