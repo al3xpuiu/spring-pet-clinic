4854adcaa780215b365d004f6263ff35a1551234
Loky <puiualexandru87@yahoo.com>
2018-08-18 10:35:21 +0200

    Created SpecialityServiceMap. closes #31

---
+++ pet-clinic-data/src/main/java/springpetclinic/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package springpetclinic.services;
+
+import springpetclinic.model.Specialty;
+
+/**
+ * Created by Loky on 18/08/2018.
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/springpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,10 @@
+package springpetclinic.services.map;
+
+import springpetclinic.model.Specialty;
+import springpetclinic.services.SpecialtyService;
+
+/**
+ * Created by Loky on 18/08/2018.
+ */
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+}
