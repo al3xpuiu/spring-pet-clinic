279fb4a4bf6dee1e76633b0dd6cc470255193c13
Loky <puiualexandru87@yahoo.com>
2018-08-28 17:50:20 +0200

    Implemented Base Entity (JPA).closes #35

--- pet-clinic-data/src/main/java/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/springpetclinic/model/BaseEntity.java
@@ -1,10 +1,19 @@
 package springpetclinic.model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by Loky on 06/08/2018.
  */
+@MappedSuperclass
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
