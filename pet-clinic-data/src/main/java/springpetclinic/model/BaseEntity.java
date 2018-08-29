9150b330c530796e8479103b8da6b78cb00e2097
Loky <puiualexandru87@yahoo.com>
2018-08-29 16:51:21 +0200

    Clear out a few bugs: @Id - it was imported the wrong annotation. There was a @Column together with @ManyToOne. They don't work together.

--- pet-clinic-data/src/main/java/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/springpetclinic/model/BaseEntity.java
@@ -1,9 +1,9 @@
 package springpetclinic.model;
 
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 /**
